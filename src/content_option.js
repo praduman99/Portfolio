0093610cb908fcff173911a838d9e5a2048377c2
praduman <praduman@zscore.co.in>
2022-12-14 12:12:47 +0530

    wed 14 dec first commit

---
+++ src/content_option.js
@@ -0,0 +1,256 @@
+const logotext = "Praduman sharma";
+const meta = {
+  title: "Praduman sharma ",
+  description: "I am a Fullstack developer in javascript Technology",
+};
+
+const introdata = {
+  title: "I’m Praduman",
+  animated: {
+    first: "I am a Fullstack Developer",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "Dedicated and efficient full stack developer in MERN STACK with 2+ years experience in application layers, presentation layers, and databases. Certified in both F/E and B/E technologies. Currently working for firm called as Coders Brain  Technology https://www.codersbrain.com ",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: " A bit about my self",
+  aboutme:
+    "Dedicated and efficient full stack developer in MERN STACK with 2+ years experience in application layers, presentation layers, and databases. Certified in both F/E and B/E technologies. Currently working for firm called as Coders Brain  Technology https://www.codersbrain.com ",
+};
+const downloadResume = {
+  title: "Download my Resume",
+  link: "",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Reactjs",
+    value: 90,
+  },
+  {
+    name: "React Native",
+    value: 90,
+  },
+  {
+    name: "Javascript",
+    value: 90,
+  },
+  {
+    name: "Redux",
+    value: 70,
+  },
+  {
+    name: "Nodejs",
+    value: 50,
+  },
+  {
+    name: "ExpressJs",
+    value: 60,
+  },
+  {
+    name: "Html",
+    value: 80,
+  },
+  {
+    name: "CSS",
+    value: 85,
+  },
+  {
+    name: "Jest",
+    value: 80,
+  },
+  {
+    name: "Xcode",
+    value: 50,
+  },
+  {
+    name: "Android studio",
+    value: 50,
+  },
+  {
+    name: "Firebase",
+    value: 60,
+  },
+  {
+    name: "App store",
+    value: 50,
+  },
+  {
+    name: "Playstore",
+    value: 50,
+  },
+  {
+    name: "Styled  Components ",
+    value: 50,
+  },
+  {
+    name: "REST API",
+    value: 50,
+  },
+  {
+    name: "Websockets",
+    value: 50,
+  },
+];
+
+const services = [
+  {
+    title: "App Development with React native",
+    description:
+      "I have a good experience in working with experts in App development using react native.",
+  },
+  {
+    title: "Website Development with Reactjs",
+    description:
+      "I have a good experience in working with cool websites which will enhance buisness standards.",
+  },
+  {
+    title: "Backend with ExpressJs or Node js ",
+    description:
+      "I have a good experience in working with Express servers using websockets and Rest Api.",
+  },
+  {
+    title: "Testing with Jest.js (UNIT testing)",
+    description:
+      "I have a experience in create automation testing scripts for react apps.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const myblogs = [
+  {
+    title: "",
+    img: "",
+    desc: "",
+    link: "",
+  },
+];
+const contactConfig = {
+  YOUR_EMAIL: "pradumanvats.000@gmail.com",
+  YOUR_FONE: "+918219333052",
+  description:
+    "If You want to get more info about me, Please feel free to  mail. I will surely reply in next 24hrs . ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+  youtube: "https://youtube.com",
+  twitch: "https://twitch.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+  downloadResume,
+};
